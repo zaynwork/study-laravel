70d12902abad112f8039bff8c37b3c9fb9585ec6
??? <119554604+zaynwork@users.noreply.github.com>
2023-05-15 01:03:13 +0800

    Fix favicon; Change logo
    - Fix favicon on certain tabs
    - Change default logo to black version

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,10 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <!-- Favicons -->
+        <link href="favicon-gray.ico" rel="icon">
+        <link href="favicon-gray.ico" rel="apple-touch-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
